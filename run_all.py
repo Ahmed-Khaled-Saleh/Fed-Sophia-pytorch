fdaf78bd7c96eda159f1458636d61cc0a1e848e0
Ahmed Khaled <aelba040@uottawa.ca>
2023-10-20 10:42:51 +0300

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         name = "sophia_exps" if args.exp_type == "regular" else "sophia-hyper-parameters"
         experiment = Experiment(
             api_key="lhdQnruUATiAZPyU7Qp2zFiVX",
-            project_name="cnn-mnist",
+            project_name="mlp-sophia-new",
             workspace="ahmed-khaled-saleh",
             )
         hyper_params = {
